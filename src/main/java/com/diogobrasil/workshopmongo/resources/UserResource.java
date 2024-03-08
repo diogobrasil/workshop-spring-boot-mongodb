90bca1479a9b3d17592034732418dfd6f30a31d6
diogo <diogobrasildasilva@gmail.com>
2024-03-07 22:13:42 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/diogobrasil/workshopmongo/resources/UserResource.java
+++ src/main/java/com/diogobrasil/workshopmongo/resources/UserResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -54,6 +55,14 @@ public class UserResource {
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable String id){
 		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@PathVariable String id, @RequestBody UserDTO obj){
+		User newObj = service.fromDTO(obj);
+		newObj.setId(id);
+		newObj = service.update(newObj);
 		return ResponseEntity.noContent().build();
 	}
 }
